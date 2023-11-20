8832e8b078a486d4979e0e3a4c9d10285800ef0c
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-11-19 22:33:03 -0300

    feat: about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import Titulo from './components/Title'
+import Title from './components/Title'
+import About from './containers/About'
 import Sidebar from './containers/Sidebar'
 
 function App() {
   return (
     <>
       <Sidebar></Sidebar>
-      <Titulo>Claudio Ferreira</Titulo>
+      <Title>Claudio Ferreira</Title>
+      <main>
+        <About></About>
+        <section>projetos</section>
+      </main>
     </>
   )
 }
---
+++ src/containers/About/index.tsx
@@ -0,0 +1,18 @@
+import Title from '../../components/Title'
+
+const About = () => (
+  <section>
+    <Title>Sobre</Title>
+    <p>
+      Desenvolvedor Full Stack, pós-graduado em Análise e Desenvolvimento de
+      Programas, com formação pela Le Wagon em Desenvolvimento Web e graduando
+      em Ciência da Computação pelo Centro Universitário União das Américas
+      Descomplica. <br />
+      Aluno do Curso Desenvolvedor Full Stack Python pela Escola de Britânica de
+      Artes Criativas e Tecnologia (EBAC). Engenheiro em Energias Renováveis
+      pelo Escola Superior de Tecnologia e Gestão - IPB, Portugal.
+    </p>
+  </section>
+)
+
+export default About

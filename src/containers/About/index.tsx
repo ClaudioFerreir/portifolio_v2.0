434d69f0fb907a9097f7319795c6fa82f9dfb067
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-11-28 11:27:14 -0300

    feat: icons added

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,5 +1,6 @@
 import Title from '../../components/Title'
 import Text from '../../components/Text'
+import { LanguageSection } from './styles'
 
 const About = () => (
   <section>
@@ -12,6 +13,19 @@ const About = () => (
       Britânica de Artes Criativas e Tecnologia (EBAC). Engenheiro em Energias
       Renováveis pelo Escola Superior de Tecnologia e Gestão - IPB, Portugal.
     </Text>
+    <LanguageSection>
+      <img src="https://img.icons8.com/color/48/000000/html-5.png" />
+      <img src="https://img.icons8.com/color/48/000000/css3.png" />
+      <img src="https://img.icons8.com/color/48/000000/bootstrap.png" />
+      <img src="https://img.icons8.com/color/48/000000/javascript.png" />
+      <img src="https://img.icons8.com/color/48/000000/ruby-programming-language.png" />
+      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/rails/rails-plain.svg" />
+      <img src="https://img.icons8.com/color/48/000000/figma.png" />
+      <img src="https://img.icons8.com/color/48/000000/visual-studio.png" />
+      <img src="https://img.icons8.com/color/48/000000/github--v1.png" />
+      <img src="https://img.icons8.com/color/48/000000/heroku.png" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" />
+    </LanguageSection>
   </section>
 )
 
---
+++ src/containers/About/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const LanguageSection = styled.div`
+  img {
+    height: 40px;
+    width: 50px;
+  }
+`
